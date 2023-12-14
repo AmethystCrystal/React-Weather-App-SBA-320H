da23aa5c3942447c4042dbc48fd381d476679ba1
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2023-12-14 17:29:00 -0500

    Added a .env file and edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
 import './App.css'
 import WeatherDisplay from './components/weatherDisplay/WeatherDisplay'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export default async function Weather() {
-  
+export default function App() {
+  useEffect( () => {
+    async function fetchData() {
+          const apiKey = import.meta.env.VITE_API_KEY;
+          console.log(apiKey)
     const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
     const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '78559492c2mshae7e73506b12476p1382e7jsnfa33ce985a0e',
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-        }
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
     };
-    
+
     try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-  
-  
-    return (
+    }
+    fetchData()
+  }, [])
+
+
+
+
+  return (
     <div>
-      <WeatherDisplay/>
+      <WeatherDisplay />
     </div>
   )
 }
--- src/components/weatherDisplay/WeatherDisplay.jsx
+++ src/components/weatherDisplay/WeatherDisplay.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Weather() {
+export default function WeatherDisplay() {
   return (
     <div>Weather</div>
   )
